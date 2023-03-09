c81c0dd38fe0ef344b92af88e8189b45fdc33c2b
DrewnioQ <debski.dev@gmail.com>
2023-03-09 18:04:31 +0100

    Add clearSketch function

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-const sketchContainer = document.querySelector(".sketch");
+const DEFAULT_SIZE2D = 16;
+
+const sketchContainer = document.getElementById("sketchContainer");
 const clearBtn = document.getElementById("clearBtn");
 
-function drawPixels(pixelNum) {
-  const pixelCount = pixelNum ** 2;
+clearBtn.onclick = () => clearSketch();
 
+function drawPixels(size2D) {
   sketchContainer.setAttribute(
     "style",
-    `grid-template-rows: repeat(${pixelNum}, 1fr);
-  grid-template-columns: repeat(${pixelNum}, 1fr)`
+    `grid-template-rows: repeat(${size2D}, 1fr);
+  grid-template-columns: repeat(${size2D}, 1fr)`
   );
 
-  for (let i = 0; i < pixelCount; i++) {
+  createDivs(size2D);
+}
+
+function createDivs(size2D) {
+  for (let i = 0; i < size2D ** 2; i++) {
     const emptyDiv = document.createElement("div");
+
     emptyDiv.classList.add("sketch-pixel");
+
+    emptyDiv.addEventListener("mousedown", () => {
+      fillColor(emptyDiv, "red");
+    });
     sketchContainer.appendChild(emptyDiv);
   }
+}
+
+function fillColor(div, color) {
+  div.style.backgroundColor = `${color}`;
+}
+
+function clearSketch() {
+  const pixels = sketchContainer.children;
+  Array.from(pixels).forEach((pixel) => {
+    pixel.style.backgroundColor = "white";
+  });
 }
 
 function main() {
